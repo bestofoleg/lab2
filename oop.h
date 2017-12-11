bffdcef90f4dda310b40c030554cf9365dd84d2e
bestofoleg <olegabramov1998@mail.ru>
2017-12-11 21:01:17 +0400

    перенёс объявление в заголовочный файл

--- oop.h
+++ oop.h
@@ -9,3 +9,23 @@ class IntegerAdder
         int getB();
         int getSum();
 };
+
+class Base
+{
+public:
+    Base();
+    ~Base();
+
+    virtual void func() const;
+};
+
+class Child: public Base
+{
+public:
+    Child();
+    ~Child();
+
+    void func() const;
+};
+
+void Function(const Base &obj);
